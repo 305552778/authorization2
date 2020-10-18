1c1033cc7bf64d60fabc4fbd905da18f12bb9961
gq <305552778@qq.com>
2020-10-18 22:54:14 +0800

    OK

---
+++ authorization2/src/main/java/com/xi/authorization/AuthorizationApplication.java
@@ -0,0 +1,14 @@
+package com.xi.authorization;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@MapperScan("com.xi.authorization.mapper")
+@SpringBootApplication
+public class AuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationApplication.class, args);
+    }
+
+}
